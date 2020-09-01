b4049dd4f6f55f9d4b7f1f7574f477957d9d67da
henriquepe <henrique.p.espindola@gmail.com>
2020-09-01 07:37:34 -0300

    configuring email

--- src/middlewares/sendding_email.ts
+++ src/middlewares/sendding_email.ts
@@ -1,33 +1,24 @@
 import nodemailer from 'nodemailer';
+import {authData} from './auth';
 
 export function sendEmail(name: string, from: string, text: string, celular: string) {
 
     let transporter = nodemailer.createTransport({
 	
 		//porta smtp
-		host: 'smtp.gmail.com',
+		host: 'smtp.umbler.com ',
 		//porta smtp do gmail
         port: 587,
         secure:false,
         
-		auth: {
-			user: 'henrique.p.espindola@gmail.com',
-			pass: 'rosa1702',
-
-		}
+		auth: authData
 });
 
 
 transporter.sendMail({
 	from: `${name} |${from}| --> <${from}>`,
-	to: "henrique.p.espindola@gmail.com",
-	subject: 'Novo Contato',
-	text: `
-		Contato de: ${name},\n
-		E-mail: ${from},\n
-		Mensagem:\n\n
-		${text}
-	`,
+	to: "adm@codingonsaucer.com",
+	subject: `Novo Contato (${name})`,
 
 	html: `
 		<h1>Contato de: ${name}</h2>
